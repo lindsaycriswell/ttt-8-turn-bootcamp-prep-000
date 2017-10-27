fee9e04157edbc90a4b1afdc05195bf15238d80e
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-27 01:20:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,6 +8,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
 def position_taken?(board, index)
   if board[index] == nil || board[index] == "" || board[index] == " "
     false
